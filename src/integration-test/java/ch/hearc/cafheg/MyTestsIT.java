f94a0dc4c61500e7a0932820e8dcd7d96cc7b1e3
Kilian Maillard <kilian.maillard@he-arc.ch>
2025-03-23 21:06:42 +0100

    Exercice 5 - Partie 2 - Add integration test for basic equality check in MyTestsIT

---
+++ src/integration-test/java/ch/hearc/cafheg/MyTestsIT.java
@@ -0,0 +1,13 @@
+package ch.hearc.cafheg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTestsIT {
+    @Test
+    public void oneEqualsOne() {
+        assertEquals(1, 1);
+    }
+
+}
